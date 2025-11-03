86ea74f280f7ed803afc80c8c2eef3b87baee4e7
SiBurnout <tcremers@telenet.be>
2025-11-03 21:04:13 +0100

    Test 1: new keyboard and map

---
+++ keyboards/00_tc/config.h
@@ -0,0 +1,27 @@
+// Copyright 2024 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define USB_VBUS_PIN GP13
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP6
+#define I2C1_SCL_PIN GP7
+
+/* RP2040- and hardware-specific config */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
+
+#define EE_HANDS
+
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true
+#define RGB_MATRIX_SPLIT
+#define SPLIT_TRANSPORT_MIRROR
+
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
+
